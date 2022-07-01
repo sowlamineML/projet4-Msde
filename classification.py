774b7c3bcd0d4a396476fd581f37c2d9857885ab
sowlamineML <108286609+sowlamineML@users.noreply.github.com>
2022-07-02 00:26:44 +0200

    Update classification.py

--- classification.py
+++ classification.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 #import numpy as np
 #from pycaret.classification import *
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #import seaborn as sns
 #from sklearn import preprocessing
 
@@ -14,12 +14,5 @@ import matplotlib.pyplot as plt
 st.title("Predict loans")
 st.header("how to predict loans")
 
-uploaded_file = st.file_uploader("Choose a file to load tabboun mouk")
-if uploaded_file is not None:
-    # Can be used wherever a "file-like" object is accepted:
-    df = pd.read_csv(uploaded_file)
-    st.write(df)
-    model_loan=pickle.load(open(r'C:\users\lenovo\desktop\msdeclassifrf.pkl', 'rb'))
-    prediction = model_loan.predict(df)
-    prediction_proba = model_loan.predict_proba(df)
+
     
